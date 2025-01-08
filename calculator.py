01eed96fec36cf3843a7fc966a2e602fd42ac2e5
Logan-Kirkpatrick <logan_kirkpatrick@icloud.com>
2025-01-08 15:41:04 -0700

    Binarycalculator

--- calculator.py
+++ calculator.py
@@ -1,2 +1,14 @@
 def binary_calculator(bin1, bin2, operator):
-    # your code here
+
+    if bin2 == "00000000" and operator == "/":
+            return "NaN"
+    for x in bin1 and bin2:
+        if x != "1" or x != "0":
+            return "Error"
+        else:
+            result = 0
+            for place, diget in enumerate(bin1[::-1]):
+                result += int(diget) * (2**place)
+            return str(result)
+            if bin2 == "0" and operator == "/":
+                return "NaN"
